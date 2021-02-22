49ff3bf23a4506eac2980018c0c4615f47dc3219
rafo-og <58552630+rafo-og@users.noreply.github.com>
2021-02-22 11:36:24 +0100

    Some modification to adjust the code to my own headers. Also the generated BOM is different to accomplish my requirements.

--- bom_csv_jlcpcb.py
+++ bom_csv_jlcpcb.py
@@ -6,46 +6,71 @@
 
 """
     @package
-    Generate a CSV BOM for use with JLCSMT service.
+    Generate a CSV BOM for use with JLC distributor.
     Components are sorted by ref and grouped by value with same footprint
     Fields are (if exist).
-    LCSC Part numbers are copied from the "LCSC Part" field, if exists.
+    LCSC Part numbers are copied from the "LCSC PN#" field, if exists.
     It is possible to hide components from the BOM by setting the 
     "JLCPCB BOM" field to "0" or "false".
 
     Output fields:
-    'Comment', 'Designator', 'Footprint', 'LCSC Part #'
+    'Reference', 'MPN #', 'Description', 'Value', 'LCSC Part #', 'Quantity', 'Footprint'
 
     Command line:
-    python "pathToFile/bom_csv_jlcsmt.py" "%I" "%O.csv"
+    python "pathToFile/bom_csv_jlcpcb.py" "%I" "%O" "<subfolder>/<filename>.csv"
 """
-
 import kicad_netlist_reader
 import csv
 import sys
 
 net = kicad_netlist_reader.netlist(sys.argv[1])
 
-with open(sys.argv[2], 'w', newline='') as f:
-    out = csv.writer(f)
-    out.writerow(['Comment', 'Designator', 'Footprint', 'LCSC Part #'])
+outFile = sys.argv[2].rpartition('/')[0] + '/' + sys.argv[3]
+
+with open(outFile, 'w') as f:
+    out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
+    out.writerow(['Reference', 'MPN #', 'Description', 'Value', 'LCSC Part #', 'Quantity', 'Footprint'])
 
     for group in net.groupComponents():
         refs = []
+        lcsc_pn = []
 
-        lcsc_pn = ""
         for component in group:
             if component.getField('JLCPCB BOM') in ['0', 'false', 'False', 'FALSE']:
                 continue
+            if component.getField('Populated') in ['0', 'false', 'False', 'FALSE']:
+                continue
             refs.append(component.getRef())
-            lcsc_pn = component.getField("LCSC Part") or lcsc_pn
+            # refs += component.getRef() + ","
+            lcsc_pn.append(component.getField("LCSC PN#"))
             c = component
+
+        if(len(lcsc_pn) == 0):
+            continue
+            
+        result = lcsc_pn.count(lcsc_pn[0]) == len(lcsc_pn)
+
+        if lcsc_pn[0] == "":
+            continue
+
+        if(not result):
+            print(refs + "don't have the same LCSC PN #")
+            print("Error in LCSC PN#")
+            exit(1)
 
         if len(refs) == 0:
             continue
 
         # Fill in the component groups common data
-        out.writerow([c.getValue() + " " + c.getDescription(), ",".join(refs), c.getFootprint().split(':')[1],
-            lcsc_pn])
+        out.writerow([
+            ",".join(refs),
+            # refs,
+            c.getField("MPN#"),
+            c.getField("Description"),
+            c.getValue(),
+            lcsc_pn[0],
+            len(refs),
+            c.getFootprint().split(':')[1]
+        ])
 
     f.close()
